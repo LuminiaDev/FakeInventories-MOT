5a86387d3dab61ed3d8b5e592f4b235f054889b9
MEFRREEX <mefrreex@gmail.com>
2025-09-01 02:08:44 +0400

    chore: make publish name lowercase

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = project.group.toString()
-            artifactId = project.name
+            artifactId = project.name.lowercase()
             version = project.version.toString()
             from(components["java"])
         }
